6b42048e8b9e228a9f5252a1b1ce79ba1b1d12e2
jamshale <jamiehalebc@gmail.com>
2018-01-04 16:42:20 -0800

    another commit

--- src/main/java/com/biobest/Application.java
+++ src/main/java/com/biobest/Application.java
@@ -2,19 +2,15 @@ package com.biobest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * Entry point for Spring boot application
+ */
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+public class Application {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
