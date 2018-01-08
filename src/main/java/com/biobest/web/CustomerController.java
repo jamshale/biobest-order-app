628e6102f9d68593a99ac2bb62de63afb61180a3
jamshale <jamiehalebc@gmail.com>
2018-01-08 00:04:53 -0800

    update

--- src/main/java/com/biobest/web/CustomerController.java
+++ src/main/java/com/biobest/web/CustomerController.java
@@ -5,8 +5,6 @@ import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class CustomerController {
